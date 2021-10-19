cb466ed9bdd8e559087f0f5f7571e0745f72bc98
Nils Berglund <nilsberglund04@gmail.com>
2021-10-19 11:07:42 +0200

    oiguygufc

--- tryparse/Program.cs
+++ tryparse/Program.cs
@@ -1,2 +1,21 @@
 ﻿using System;
 
+int gold = 100;
+string item;
+string howMany;
+
+Console.WriteLine("You have " + gold + " gold. What du you want to buy? Skooma(10 gold), Potion (30 gold) or Helmet (70 gold)");
+item = Console.ReadLine();
+
+item = item.ToLower();
+
+if(item == "skooma" || item == "potion" || item == "helmet")
+{
+    Console.WriteLine("How may would you like to buy?");
+    howMany = Console.ReadLine();
+    
+    
+
+}
+
+Console.ReadLine();
